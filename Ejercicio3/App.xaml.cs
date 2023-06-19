52139f1a6b8901dea83982298b827c415662c103
Emers <Emers@DESKTOP-RC7M2CL>
2023-06-19 15:48:04 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio3/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Ejercicio3;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Ejercicio3/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejercicio3;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void resolver_Clicked(object sender, EventArgs e)
+    {
+		if(!string.IsNullOrEmpty(datoa.Text)&& !string.IsNullOrEmpty(datob.Text))
+		{
+			var dato1 = Convert.ToDouble(datoa.Text);
+			var dato2 = Convert.ToDouble(datob.Text);
+			double resul = Math.Pow(dato1 + dato2, 2) / 2;
+			resultado.Text =Convert.ToString(resul);
+			 
+		}
+		else
+		{
+			DisplayAlert("Eror", "Introduzca el numero faltante ciego", "Adoptado;)");
+		}
+    }
+}
+
